60c5a767f66e65ce16351a8b643d3cf43a2da45a
NiallS4 <44320530+NiallS4@users.noreply.github.com>
2019-02-22 18:36:45 +0000

    Add files via upload

--- Word.java
+++ Word.java
@@ -1,8 +1,9 @@
 public class Word {
 	public static boolean containsLetter(String word, char letter) {
 		for(int i = 0; i < word.length(); i++) {
-			if (word.charAt(i) == letter)
+			if (word.charAt(i) == letter) {
 				return true;
+			}
 		}
 		return false;
 	}
