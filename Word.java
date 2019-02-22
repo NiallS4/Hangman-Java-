f71bda7f49d962787848509d4b42651a800dedec
NiallS4 <44320530+NiallS4@users.noreply.github.com>
2019-02-22 18:33:57 +0000

    Add files via upload

--- Word.java
+++ Word.java
@@ -1,5 +1,5 @@
 public class Word {
-	
+
 	public static boolean containsLetter(String word, char letter) {
 		for(int i = 0; i < word.length(); i++) {
 			if (word.charAt(i) == letter)
@@ -53,5 +53,4 @@ public class Word {
 		int randomIndex = (int) (Math.random() * words.length);
 		return words[randomIndex];
 	}
-
 }
